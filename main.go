269e4e43021613c825dd5a5f81a7bc25d3b79fff
Derek <derek.kinzo@alumni.harvard.edu>
2020-08-09 21:53:38 -0600

    Initial add map html, js files and bootstrap

--- main.go
+++ main.go
@@ -1,16 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+const (
+	htmlExtension = ".html"
+	mapPageName   = "map"
+)
+
+var templates = template.Must(template.ParseGlob("templates/*.html"))
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+	err := templates.ExecuteTemplate(w, "map.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
 	http.HandleFunc("/", indexHandler)
+	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./static"))))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
