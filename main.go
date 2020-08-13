06d5fddb991b8efb0f1306eaeaad67efb91501e7
Derek <derek.kinzo@alumni.harvard.edu>
2020-08-12 22:46:24 -0600

    Initial add map api

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"html/template"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
+
+func main() {
+	http.HandleFunc("/api/map", mapAPI)
+	http.HandleFunc("/articles", allArticles)
+	http.HandleFunc("/", mapHandler)
+	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./static"))))
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
 
 const (
 	htmlExtension = ".html"
@@ -13,7 +24,7 @@ const (
 
 var templates = template.Must(template.ParseGlob("templates/*.html"))
 
-func indexHandler(w http.ResponseWriter, r *http.Request) {
+func mapHandler(w http.ResponseWriter, r *http.Request) {
 	err := templates.ExecuteTemplate(w, "map.html", nil)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -21,8 +32,25 @@ func indexHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func main() {
-	http.HandleFunc("/", indexHandler)
-	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./static"))))
-	log.Fatal(http.ListenAndServe(":8080", nil))
+//Article is an example json struct
+type Article struct {
+	Title   string `json:"Title"`
+	Desc    string `json:"dec"`
+	Content string `json:"content"`
+}
+
+//Articles is an array of Article
+type Articles []Article
+
+func allArticles(w http.ResponseWriter, r *http.Request) {
+	articles := Articles{
+		Article{Title: "Test title", Desc: "Test desc", Content: "Test content"},
+	}
+	fmt.Println("Endpoint hit: All articles")
+	json.NewEncoder(w).Encode(articles)
+}
+
+func mapAPI(w http.ResponseWriter, r *http.Request) {
+	geojson, _ := ioutil.ReadFile("./static/brazil-municipalities.json")
+	fmt.Fprintf(w, string(geojson))
 }
